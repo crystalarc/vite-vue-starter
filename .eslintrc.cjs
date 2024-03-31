1c38db6829b0689228efa258ef2930f12199139d
Lenka Kadlecová <kadlecovall@seznam.cz>
2024-03-31 16:48:43 +0200

    feat: Setup Prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,13 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/typescript/recommended'],
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
   parserOptions: {
     ecmaVersion: 2020,
   },
